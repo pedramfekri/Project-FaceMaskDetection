544ff989f75b2f7397004174c0922de8d09358e0
Ahmed Alagha <ahmedn.alagha@hotmail.com>
2020-11-16 17:26:22 +0400

    Error fixed

--- Model/SimpleCNN2.py
+++ Model/SimpleCNN2.py
@@ -6,7 +6,7 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5) #3-channel image
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(6 * 110 * 110, 10)
+        self.fc1 = nn.Linear(6 * 110 * 110, 3)
 
 
     def forward(self, x):
