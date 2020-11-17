eb0046d7478937da332d98442ffb1aaa1876a17a
Ahmed Alagha <ahmedn.alagha@hotmail.com>
2020-11-17 10:50:34 +0400

    fixed error

--- Dataset/ImageLoader.py
+++ Dataset/ImageLoader.py
@@ -33,28 +33,28 @@ def load_data(root_path, dir, test_split, val_split, batch_size):
     train_dataset, test_dataset, val_dataset = td.random_split(data,
                                                [train_size, test_size, val_size])
 
-    data_loader_train = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=False, num_workers=2)
-    data_loader_test  = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True, drop_last=False, num_workers=2)
-    data_loader_val   = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True, drop_last=False, num_workers=2)
+    data_loader_train = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=False, num_workers=0)
+    data_loader_test  = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True, drop_last=False, num_workers=0)
+    data_loader_val   = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True, drop_last=False, num_workers=0)
 
     return data_loader_train, data_loader_test, data_loader_val
 
 
-root_path = '/home/pedram/PycharmProjects/Project-FaceMaskDetection/Dataset/'
-dir = 'Dataset-3Class-Sample'
-
-train, test, val = load_data(root_path, dir, 0.3, 0.1, 32)
-print(len(train))
-print(len(test))
-print(len(val))
-
-train = iter(train)
-a, label = next(train)
-im = a[0].numpy()
-print('label')
-print(label)
-print(im.shape)
-im = np.transpose(im, [1, 2, 0])
-print(im.shape)
-plt.imshow(im)
-plt.show()
+# root_path = '/home/pedram/PycharmProjects/Project-FaceMaskDetection/Dataset/'
+# dir = 'Dataset-3Class-Sample'
+#
+# train, test, val = load_data(root_path, dir, 0.3, 0.1, 32)
+# print(len(train))
+# print(len(test))
+# print(len(val))
+#
+# train = iter(train)
+# a, label = next(train)
+# im = a[0].numpy()
+# print('label')
+# print(label)
+# print(im.shape)
+# im = np.transpose(im, [1, 2, 0])
+# print(im.shape)
+# plt.imshow(im)
+# plt.show()
