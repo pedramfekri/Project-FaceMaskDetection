2d6b2dca27f16ae16d1cee36d2cd10a8355d066d
Pedram Fekri <Pedram.fekri@gmail.com>
2020-12-03 16:06:10 -0500

    customeDataLoader Train_KFold

---
+++ Dataset/customeDataLoader.py
@@ -0,0 +1,47 @@
+import torch
+from __future__ import print_function, division
+import os
+import torch
+import pandas as pd
+from skimage import io, transform
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+
+
+class FaceLandmarksDataset(Dataset):
+    """Face Landmarks dataset."""
+
+    def __init__(self, csv_file, root_dir, dataframe, transform=None):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.landmarks_frame = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
+        self.df = dataframe
+
+    def __len__(self):
+        return self.df.shape[0]
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_name = os.path.join(self.root_dir,
+                                self.df.iloc[idx, 0])
+        image = io.imread(img_name)
+        label = self.df.iloc[idx, 1:]
+        label = np.array([label])
+        label = label.astype('float').reshape(-1, 2)
+        sample = {'image': image, 'label': label}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
